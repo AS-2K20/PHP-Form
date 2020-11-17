49bfb8b18519b88651f45493edefe03f719ecaeb
AS-2K20 <amudhan.psgcas@gmail.com>
2020-11-17 14:24:49 +0530

    Added some code for FireBase Deployment

--- index.php
+++ index.php
@@ -11,6 +11,15 @@
     <link rel="stylesheet" href="index.css">
     <!-- JQuery CDN -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- The core Firebase JS SDK is always required and must be listed first -->
+    <script src="/__/firebase/8.0.2/firebase-app.js"></script>
+
+    <!-- TODO: Add SDKs for Firebase products that you want to use
+         https://firebase.google.com/docs/web/setup#available-libraries -->
+
+    <!-- Initialize Firebase -->
+    <script src="/__/firebase/init.js"></script>
     
 </head>
 
